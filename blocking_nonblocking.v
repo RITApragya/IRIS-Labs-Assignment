0398a80d070a72c7ef633dce09a2fbcca273ff5c
RITApragya <111945991+RITApragya@users.noreply.github.com>
2023-05-26 19:13:01 +0530

    Create blocking_nonblocking.v

---
+++ blocking_nonblocking.v
@@ -0,0 +1,36 @@
+/*
+initial begin
+	a = #5 2’d5;
+	b = #5 2’d5;
+	c = #5 2’d5;
+*/
+
+/*
+initial begin
+	a <= #5 2’d5;
+	b <= #5 2’d5;
+	c <= #5 2’d5;
+
+*/
+
+
+/*
+a=1;b=2;c=3;
+always @ (*) begin
+	a = 5;
+	b = a;
+	c = a;
+end 
+
+*/
+
+
+/*
+a=1;b=2;c=3;
+always @ (*) begin
+	a <= 5;
+	b <= a;
+	c <= b;
+end 
+
+*/
